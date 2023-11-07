48a37399618b7b611199ca1c1b5dfec54c1d53e8
Raymond Zhang <RaymondTZhang@outlook.com>
2023-11-07 18:10:43 -0500

    Finished OrdersController

---
+++ blue.ink.Api/Controllers/OrdersController.cs
@@ -0,0 +1,107 @@
+//The command in instruction wouldn't run and professor said to build it ourselves
+//We copied the CatalogControllers file and used Google to build it
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using blue.ink.Data;
+using blue.ink.Domain.Orders;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace blue.ink.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // Updated route
+    public class OrdersController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public OrdersController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public async Task<IActionResult> GetOrders()
+        {
+            var orders = await _context.Orders.ToListAsync();
+            return Ok(orders);
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(order);
+        }
+
+        // PUT: api/Orders/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder(int id, Order order)
+        {
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Orders
+        [HttpPost]
+        public async Task<IActionResult> PostOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetOrder), new { id = order.Id }, order);
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return Ok(order);
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+    }
+}
